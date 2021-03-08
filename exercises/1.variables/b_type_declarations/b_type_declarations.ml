7a47fc1dff0566eb1848aa8d43eb898774f2c775
Sonja Heinze <sonjaleaheinze@gmail.com>
2021-03-08 12:31:19 +0100

    Add the first set of exercises: variables
    Every exercise is inside its own subfolder to guarantee independent builds.
    
    Co-authored-by: Gargi Sharma <gs051095@gmail.com>

---
+++ exercises/1.variables/b_type_declarations/b_type_declarations.ml
@@ -0,0 +1,6 @@
+(* OCaml is strongly typed; types are often inferred by the compiler.
+It's possible to declare types inline as done in this exercise,
+but that's not a common practice. *)
+
+let x : int = 5
+let y : float = "hello"
